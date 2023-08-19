0a399d6522964bcb65879d51b98de058670b61b1
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-19 18:46:16 +0300

    done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+
+Item.pay_rate = 0.8
+
+def test_apply_discount():
+    item1.apply_discount()
+    assert item1.price == 8000.0
+    assert item2.price == 20000
+
