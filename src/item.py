52244b3642cc4c3c6540a9415b4c57eff9ac71dd
VladimirByel <122753519+VladimirByel@users.noreply.github.com>
2023-08-19 18:56:10 +0300

    space item.py

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+    
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item
@@ -14,12 +15,14 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
         """
         return self.price * self.quantity
 
+    
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
