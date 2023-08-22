08d950ad5d4f37bda533c6a056033a79488c6b4f
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-22 20:42:11 +0300

    item done

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -10,10 +13,27 @@ class Item:
         """
         Создание экземпляра класса item
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            self.__name = value[0:9]
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                Item.all.append(Item(row['name'], row['price'], row['quantity']))
 
     
     def calculate_total_price(self) -> float:
@@ -28,3 +48,14 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @staticmethod
+    def string_to_number(number):
+        if '.' in number:
+            return int(float(number))
+        elif number.isnumeric():
+            return int(number)
+        else:
+            print('not a number')
+
+
