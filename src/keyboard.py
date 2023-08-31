d300b728a4128103664a767fe4d2627a895b0507
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-31 10:34:02 +0300

    ?

---
+++ src/keyboard.py
@@ -0,0 +1,18 @@
+from src.item import Item
+
+
+class MixinLog:
+
+    def change_lang(self):
+        if self.language == 'EN':
+            self.language = 'RU'
+        else:
+            self.language = 'EN'
+        return self
+
+
+class Keyboard(Item, MixinLog):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.language = 'EN'
